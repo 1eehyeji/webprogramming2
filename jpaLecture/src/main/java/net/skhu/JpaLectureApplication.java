ade719592559daf3833558db4488140ee7067514
HYEJI <lwiiiiii@naver.com>
2018-11-29 23:50:03 +0900

    jpa 강의예제 구현

---
+++ jpaLecture/src/main/java/net/skhu/JpaLectureApplication.java
@@ -0,0 +1,12 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLectureApplication.class, args);
+	}
+}
