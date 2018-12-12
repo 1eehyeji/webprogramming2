4e8af5a05bc98bb94a5917949a6c913f122b8804
HYEJI <lwiiiiii@naver.com>
2018-12-12 21:06:11 +0900

    valid 어노테이션 예제 프로젝트 생성

---
+++ formValid/src/main/java/net/skhu/FormValidApplication.java
@@ -0,0 +1,12 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormValidApplication.class, args);
+	}
+}
